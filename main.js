e94acf6f7f2a80da10481c5d000a33b7dc9d19d4
Jason <bjason@gmail.com>
2014-07-27 20:33:56 -0700

    Added lib, and namespcaded

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-var loadScript = function(src){
 
+var jssandbox ={};
+
+
+jssandbox.Libs = [
+{name: "jQuery", version: "2.1.1", src: "jquery2_1_1.js", desc: "jQuery..."}
+];
+
+
+
+jssandbox.loadScript = function(src){
 
    var head= document.getElementsByTagName('head')[0];
    var script= document.createElement('script');
