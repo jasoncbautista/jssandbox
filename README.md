718b61538571dd56c04f32b3395dcd329428faf4
Jason <bjason@gmail.com>
2014-09-20 19:58:42 -0700

    Added simple jquery script

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ jssandbox
 
 
 # An extremely simple sandbox for testing js
+
+
+[live version](url)
+
+
+
+# TODOs
+- move away from using Jquery
--- main.js
+++ main.js
@@ -1,36 +1,32 @@
-
 var jssandbox ={};
 
-
 jssandbox.Libs = [
-{name: "jQuery", version: "2.1.1", src: "jquery2_1_1.js", desc: "jQuery..."}
+    {name: "jQuery", version: "2.1.1", src: "libs/jquery2_1_1.min.js", desc: "jQuery..."}
 ];
 
-
-
 jssandbox.loadScript = function(src){
-
    var head= document.getElementsByTagName('head')[0];
    var script= document.createElement('script');
+
    script.type= 'text/javascript';
    script.src= src;
    head.appendChild(script);
-
 };
 
+jssandbox.showScript = function(scriptObj){
+};
 
-
+// TODO: figure out way to  load scripts in their own namespaces
 $(function){
+    // Bind the click event to add a new script based on a url
     $("#loadExternalScript").click(function(){
         var scriptSRC = $("#scriptName").val()
         jssandbox.loadScript(scriptSRC);
     });
 
    var libTemplate =  _.template( $("#lib_entry")  ) ;
-   
    _.each(jssandbox.Libs, function(lib){
       var libEntry = $( libTemplate(lib)  );
-      
          libEntry.find(".clickToAdd").click(function(){
             jssandbox.loadScript(lib.src);
          });
