a171b08ef369426ff62216560d8b971df0db491d
Jason <bjason@gmail.com>
2014-09-20 20:19:54 -0700

    On click, load script

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var jssandbox ={};
 
 jssandbox.Libs = [
     {name: "jQuery", version: "2.1.1", src: "libs/jquery2_1_1.min.js", desc: "jQuery..."}
-    , {name: "Underscore", version: "1.7.0", src: "libs/underscore-min_1_7_0.js", desc: "Underscore..."}
+    , {name: "Underscore", version: "1.7.0", src: "libs/underscore-min_1_7_0.js"
+        , desc: "Underscore..."}
 ];
 
 jssandbox.loadScript = function(src){
@@ -19,19 +20,25 @@ jssandbox.showScript = function(scriptObj){
 
 // TODO: figure out way to  load scripts in their own namespaces
 $(function(){
-    alert('hi');
+
     // Bind the click event to add a new script based on a url
     $("#loadExternalScript").click(function(){
         var scriptSRC = $("#scriptName").val()
         jssandbox.loadScript(scriptSRC);
     });
 
-   var libTemplate =  _.template( $("#lib_entry")  ) ;
+   var libTemplate =  _.template( $("#lib_entry").html()  ) ;
+
    _.each(jssandbox.Libs, function(lib){
       var libEntry = $( libTemplate(lib)  );
          libEntry.find(".clickToAdd").click(function(){
             jssandbox.loadScript(lib.src);
          });
       libEntry.appendTo($("body"));   
+      libEntry.click(function(){
+           console.log("loading...", lib.name);
+           jssandbox.loadScript(lib.src);
+      });
    });
+
 });
